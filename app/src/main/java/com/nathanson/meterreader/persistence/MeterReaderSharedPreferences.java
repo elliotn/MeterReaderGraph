8c2ea3db2c2479183eb983fe79b424106440acc8
Elliot Nathanson <elliot.nathanson@gmail.com>
2015-09-14 14:20:23 -0600

    Created shared prefs framework.

---
+++ app/src/main/java/com/nathanson/meterreader/persistence/MeterReaderSharedPreferences.java
@@ -0,0 +1,65 @@
+package com.nathanson.meterreader.persistence;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+
+public class MeterReaderSharedPreferences {
+    private static final String TAG = "MRSharedPreferences";
+
+    private static final String SETTINGS_URL = "SETTINGS_URL";
+    private static final String SETTINGS_ALERT_THRESHOLD = "SETTINGS_ALERT_THRESHOLD";
+
+
+
+    private volatile static MeterReaderSharedPreferences INSTANCE;
+    private SharedPreferences mSharedPrefs;
+
+    private MeterReaderSharedPreferences() {}
+
+    public static MeterReaderSharedPreferences getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (MeterReaderSharedPreferences.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new MeterReaderSharedPreferences();
+                    INSTANCE.mSharedPrefs =  PreferenceManager.getDefaultSharedPreferences(context);
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public void setUrl(String url) {
+        INSTANCE.mSharedPrefs
+                .edit()
+                .putString(SETTINGS_URL, url)
+                .apply();
+
+    }
+
+    public String getUrl() {
+        return INSTANCE.mSharedPrefs
+                .getString(SETTINGS_URL, "");
+    }
+
+
+    // TODO: for future usage.
+    public void setUsageAlertThreshold(int threshold) {
+        INSTANCE.mSharedPrefs
+                .edit()
+                .putInt(SETTINGS_ALERT_THRESHOLD, threshold)
+                .apply();
+    }
+
+    // TODO: for future usage.
+    public int getUsageAlertThreshold() {
+        return INSTANCE.mSharedPrefs
+                .getInt(SETTINGS_ALERT_THRESHOLD, -1);
+    }
+
+
+
+}
+

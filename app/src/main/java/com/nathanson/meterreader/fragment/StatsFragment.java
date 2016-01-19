9ceb278c5044e8f649388cf455e6fdab60ea7a97
Elliot Nathanson <elliot.nathanson@gmail.com>
2016-01-19 16:59:00 -0700

    Calculate bill comparison.

--- app/src/main/java/com/nathanson/meterreader/fragment/StatsFragment.java
+++ app/src/main/java/com/nathanson/meterreader/fragment/StatsFragment.java
@@ -1,13 +1,30 @@
+/*
+ * Copyright (C) 2015 Elliot Nathanson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nathanson.meterreader.fragment;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.nathanson.meterreader.MeterReaderApplication;
@@ -17,7 +34,6 @@ import com.nathanson.meterreader.data.Meter;
 import com.nathanson.meterreader.data.MeterReading;
 import com.nathanson.meterreader.fetch.DataFetcher;
 
-import java.util.Calendar;
 import java.util.List;
 
 import butterknife.Bind;
@@ -97,6 +113,8 @@ public class StatsFragment extends BaseFragment
                 ButterKnife.bind(this, statsLayout);
 
                 billComparisonCalculate.setOnClickListener(this);
+                billComparisonStartDate.setOnClickListener(this);
+                billComparisonEndDate.setOnClickListener(this);
 
                 return statsLayout;
         }
@@ -142,10 +160,52 @@ public class StatsFragment extends BaseFragment
                                 calcBillComparison(mMeters.get(0));
                                 break;
 
+                        case R.id.billComparisonStartDate:
+                                resetBillComparisonResults();
+                                showDatePicker(billComparisonStartDate.getText().toString(), mStartDateListener);
+                                break;
+
+                        case R.id.billComparisonEndDate:
+                                resetBillComparisonResults();
+                                showDatePicker(billComparisonEndDate.getText().toString(), mEndDateListener);
+                                break;
+
                         default:
                                 // do nothing.
                                 break;
                 }
+        }
+
+
+        private DatePickerDialog.OnDateSetListener mStartDateListener =
+                new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                String formattedTimeStamp = String.format(MeterReading.TIMESTAMP_FORMAT, monthOfYear + 1, dayOfMonth, year);
+                                billComparisonStartDate.setText(formattedTimeStamp);
+                        }
+                };
+
+
+        private DatePickerDialog.OnDateSetListener mEndDateListener =
+                new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                String formattedTimeStamp = String.format(MeterReading.TIMESTAMP_FORMAT, monthOfYear + 1, dayOfMonth, year);
+                                billComparisonEndDate.setText(formattedTimeStamp);
+                        }
+                };
+
+        private void showDatePicker(String date, DatePickerDialog.OnDateSetListener dateListener) {
+                String[] dateArray = date.split("/");
+
+                new DatePickerDialog(getActivity(),
+                        dateListener,
+                        Integer.valueOf(dateArray[2]),
+                        Integer.valueOf(dateArray[0]) - 1 ,
+                        Integer.valueOf(dateArray[1]))
+                .show();
+
         }
 
         private void calcLast30Days(Meter meter) {
@@ -243,7 +303,8 @@ public class StatsFragment extends BaseFragment
                 billComparisonEndDate.setText(lastReading);
         }
 
-        private void reset() {
-                // TODO: clean calculated stuff when calculate is called.
+        private void resetBillComparisonResults() {
+                billComparisonUsageData.setText("");
+                billComparisonDailyAveData.setText("");
         }
 }
